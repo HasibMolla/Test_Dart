6b64df1f752dd6497b111807220034404553b574
mdhasibmolla <mdhasibm700@gmail.com>
2024-10-19 02:10:18 +0600

    Write a program to swap two numbers in dart

---
+++ swap.dart
@@ -0,0 +1,14 @@
+void main(){
+  int a = 5;
+  int b= 6;
+  int c;
+  print("Before Swaping");
+  print (a);
+  print (b);
+  c= a;
+  a= b;
+  b=c;
+  print("After Swaping");
+  print(a);
+  print(b);
+}
