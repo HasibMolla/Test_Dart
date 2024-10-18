236ae6ba0ef92df018bbbe7e9b8abb4a29d8ffab
mdhasibmolla <mdhasibm700@gmail.com>
2024-10-18 22:18:46 +0600

    dartdatatypesNumbers

---
+++ Numbers.dart
@@ -0,0 +1,15 @@
+void main(){
+  // When you need to store numeric value on dart ,you can use either int or double.
+  // both int and int and doubles are subtypes of num.
+  int num1 = 100;
+  int num2  = 200;
+  num num3 = 100.100;
+  num num4 = 200.200;
+  num sum = num3+ num4+num2+num1;
+  print("Num1 is $num1");
+  print("Num2 is $num2");
+  print("Num3 is $num3");
+  print("Num4 is $num4");
+  print("Sum is $sum");
+
+}
