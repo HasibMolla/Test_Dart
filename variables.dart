bf1feb85d15a6db089d7eb56046da080c5e33389
mdhasibmolla <mdhasibm700@gmail.com>
2024-10-18 21:38:44 +0600

    Initial commit

---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name ="Md Hasib Molla";
+  String address = "Khulna Bangladesh";
+  int age = 24;
+  double height = 5.7;
+  bool isMarried = false;
+  print("Name is: $name");
+  print("Address is: $address");
+  print("Age is: $age");
+  print("Height is: $height");
+  print("Married Status is: $isMarried");
+
+}
